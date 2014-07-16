4c062f428d26cffc6684411e9d1532b3ccb07304
Yu-Jie Lin <livibetter@gmail.com>
2014-07-16 14:24:10 +0800

    fix warnings reported with -Wall -Wextra

--- bubbles.c
+++ bubbles.c
@@ -7,6 +7,7 @@
 #ifdef __STDC__
 #include <stdlib.h>
 #endif
+#include <unistd.h>
 
 #define SURFACE		((LINES) / 5)
 #define NOBUBBLE		' '
@@ -20,13 +21,74 @@ struct bubble {
 };
 
 
-main (argc, argv) 
+float frand ()		/** in range 0.0 .. 1.0	*/
+{
+	return ((float) (rand () & 0377) / 256.0);
+} /* frand */
+
+void new (pb)			/** make a new one	*/
+struct bubble *pb;
+{
+	int size;
+
+	pb->y = LINES - 1;
+	pb->x = (int) ((frand () * (float) (COLS - 1)) + 0.5);
+	pb->speed = (int) (frand () * 3.0) + 1;
+	size = (int) (frand () * 4.0);
+
+	switch (size) {
+	case 1: pb->image = '.';
+		break;
+	case 2: pb->image = 'o';
+		break;
+	case 3:	pb->image = 'O';
+		break;
+	default:
+		pb->image = NOBUBBLE;
+	} /* switch */
+} /* new */
+
+void pop (pb)			/** pop it	*/
+struct bubble *pb;
+{
+	switch (pb->image) {
+	case '.':
+		mvaddch (SURFACE, pb->x, '_');
+		break;
+	case 'o':
+		mvaddch (SURFACE, pb->x, 'v');
+		break;
+	case 'O':
+		mvaddch (SURFACE, pb->x, '*');
+		mvaddch (SURFACE - 1, pb->x, '.');
+		break;
+	default:
+		return;
+	} /* switch */
+
+	pb->image = NOBUBBLE;
+} /* pop */
+
+#ifdef __STDC__
+void abortHandle (signum)
+int signum;
+#else
+abortHandle ()
+#endif
+{
+	(void)(signum); /* avoiding "unused parameter" */
+	move (LINES - 1, 0);
+	refresh ();
+	endwin ();
+	exit (0);
+} /* abortHandle */
+
+int main (argc, argv) 
 int argc;
 char *argv[];
 {
 	static struct bubble *bubbles;
 	int c;
-	extern char *optarg;
 	/* update interval in ms */
 	int delay = 500;
 	int quantity;
@@ -94,64 +156,3 @@ char *argv[];
 	} /* for */
 	endwin ();
 } /* main */
-
-float frand ()		/** in range 0.0 .. 1.0	*/
-{
-	return ((float) (rand () & 0377) / 256.0);
-} /* frand */
-
-new (pb)			/** make a new one	*/
-struct bubble *pb;
-{
-	int size;
-
-	pb->y = LINES - 1;
-	pb->x = (int) ((frand () * (float) (COLS - 1)) + 0.5);
-	pb->speed = (int) (frand () * 3.0) + 1;
-	size = (int) (frand () * 4.0);
-
-	switch (size) {
-	case 1: pb->image = '.';
-		break;
-	case 2: pb->image = 'o';
-		break;
-	case 3:	pb->image = 'O';
-		break;
-	default:
-		pb->image = NOBUBBLE;
-	} /* switch */
-} /* new */
-
-pop (pb)			/** pop it	*/
-struct bubble *pb;
-{
-	switch (pb->image) {
-	case '.':
-		mvaddch (SURFACE, pb->x, '_');
-		break;
-	case 'o':
-		mvaddch (SURFACE, pb->x, 'v');
-		break;
-	case 'O':
-		mvaddch (SURFACE, pb->x, '*');
-		mvaddch (SURFACE - 1, pb->x, '.');
-		break;
-	default:
-		return;
-	} /* switch */
-
-	pb->image = NOBUBBLE;
-} /* pop */
-
-#ifdef __STDC__
-void abortHandle (signum)
-int signum;
-#else
-abortHandle ()
-#endif
-{
-	move (LINES - 1, 0);
-	refresh ();
-	endwin ();
-	exit (0);
-} /* abortHandle */
